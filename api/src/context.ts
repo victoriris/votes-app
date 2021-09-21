ee1bdd7e2e088caf217209dd3a6d91c3e022a7d0
Victor Iris <victormidp@gmail.com>
2021-09-20 21:16:26 -0500

    feat: setup prisma

--- api/src/context.ts
+++ api/src/context.ts
@@ -1,9 +1,12 @@
+import { PrismaClient } from "@prisma/client";
 import { ExpressContext } from "apollo-server-express/dist/ApolloServer";
 import { Request, Response } from "express";
+import { prisma } from "./clients";
 
 export interface Context {
 	request: Request;
 	response: Response;
+	prisma: PrismaClient;
 }
 
 export async function createContext(
@@ -13,5 +16,6 @@ export async function createContext(
 		...request,
 		response: request.res,
 		request: request.req,
+		prisma
 	};
 }
